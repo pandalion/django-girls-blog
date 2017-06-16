f372c4507192e0fd1da0ad3c6fee701bf222947a
Paula Norris <thepandalion@gmail.com>
2017-06-16 08:02:05 +0100

    my django girls app, first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pnorris/Sites/django-girls/django-girls/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ python_intro.py
@@ -0,0 +1,6 @@
+if name == 'Panda':
+    print('Hey Panda!')
+elif name == 'Lion':
+    print('Hey Lion!')
+else:
+    print('Hey anonymous!')
